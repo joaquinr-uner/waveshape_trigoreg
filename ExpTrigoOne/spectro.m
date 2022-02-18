290f510e88263d87c0036907d00ce5416104ef5f
joaquinr-uner <70167021+joaquinr-uner@users.noreply.github.com>
2022-02-17 23:07:34 -0300

    Add files via upload

--- ExpTrigoOne/spectro.m
+++ ExpTrigoOne/spectro.m
@@ -1,6 +1,6 @@
 function h1 = spectro(F,t,f)
-%UNTITLED8 Summary of this function goes here
-%   Detailed explanation goes here
+% Function that plots a spectrogram given the STFT of a signal
+% and the time (t) and frequency (f) axis
 if nargin<3
     imagesc(abs(F).^2), axis xy
 else
